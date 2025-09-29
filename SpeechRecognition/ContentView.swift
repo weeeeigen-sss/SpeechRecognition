5fa0da263d42b44772f645d46bf061ea50e93803
Yusaku Eigen <yusaku.eigen@sony.com>
2025-09-29 11:10:32 +0900

    Initial Commit

---
+++ SpeechRecognition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeechRecognition
+//
+//  Created by Eigen, Yusaku (SSS) on 2025/09/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeechRecognition/SpeechRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognitionApp.swift
+//  SpeechRecognition
+//
+//  Created by Eigen, Yusaku (SSS) on 2025/09/29.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
